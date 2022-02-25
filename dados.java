31dc190cf571415946d4e6b46f0c14bbf3e9d990
viniciuslorencio <vini.tim@hotmail.com>
2022-02-25 10:59:39 -0300

    Exercicio 1

---
+++ dados.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+public class dados {
+    public  static void dados(String[] args) {
+
+        //Calculadora
+        System.out.println("Exercicio calculadora ");
+       Exercicio1.soma(7, 88);
+       Exercicio1.subtracao(5, 7);
+       Exercicio1.multiplicacao(7, 6);
+       Exercicio1.divisao(6, 9);
+
+    }
+}
---
+++ model/Main/main.java
@@ -0,0 +1,31 @@
+package br.com.dio.model.Main;
+
+public class main {
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 1452;
+        short s2 = 1469;
+
+        int c2 = 86;
+        int c4 = 563;
+
+        long l1 = 555555555555558l;
+        long l2 = 1564864564512318468l;
+
+        float f1 = 4.5f;
+        float f3 = 7.5f;
+
+        double d2 = 48.859;
+        double d1 = 75.984;
+
+        boolean bh3 = true;
+        boolean bh2 = false;
+
+        System.out.println(bh2);
+    }
+
+
+}
